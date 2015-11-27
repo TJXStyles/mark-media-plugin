8678dcde8194fa884748f93306edace7ef1ce64c
Jack Cooc <jack.cooc@gmail.com>
2015-11-27 13:24:45 -0500

    checking the checkbox now stays checked

--- admin/partials/mark-media-plugin-admin-display.php
+++ admin/partials/mark-media-plugin-admin-display.php
@@ -15,12 +15,21 @@
 <div class="wrap">
 	<h2><?php echo esc_html(get_admin_page_title()); ?></h2>
 	<form method="post" name="cleanup_options" action="options.php">
-	<?php settings_fields($this->plugin_name); ?>
+		<?php
+		//Grab all options
+		$options = get_option($this->plugin_name);
+		// body_slug
+		$body_class_slug = $options['body_class_slug'];
+		?>
+		<?php
+		settings_fields($this->plugin_name);
+		do_settings_sections($this->plugin_name);
+		?>
 		<!-- Add slug to the body as a class "page-<slug>" -->
 		<fieldset>
 			<legend class="screen-reader-text"><span><?php _e('Add Post, page or product slug to body class', $this->plugin_name); ?></span></legend>
 			<label for="<?php echo $this->plugin_name; ?>-body_class_slug">
-				<input type="checkbox" id="<?php echo $this->plugin_name;?>-body_class_slug" name="<?php echo $this->plugin_name; ?>[body_class_slug]" value="1" />
+				<input type="checkbox" id="<?php echo $this->plugin_name; ?>-body_class_slug" name="<?php echo $this->plugin_name; ?>[body_class_slug]" value="1" <?php checked($body_class_slug, 1); ?>  />
 				<span><?php esc_attr_e('Add Post slug to body class', $this->plugin_name); ?></span>
 			</label>
 		</fieldset>
