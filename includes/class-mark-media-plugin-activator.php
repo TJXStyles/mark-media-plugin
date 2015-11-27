50a80cffcedc111cdd33e2d68c6ed3c9005d4cd0
Jack Cooc <jack.cooc@gmail.com>
2015-11-27 12:13:13 -0500

    initial commit

---
+++ includes/class-mark-media-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://www.jackcooc.com
+ * @since      1.0.0
+ *
+ * @package    Mark_Media_Plugin
+ * @subpackage Mark_Media_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Mark_Media_Plugin
+ * @subpackage Mark_Media_Plugin/includes
+ * @author     Jack <jackc@markmedia.co>
+ */
+class Mark_Media_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-mark-media-plugin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://www.jackcooc.com
+ * @since      1.0.0
+ *
+ * @package    Mark_Media_Plugin
+ * @subpackage Mark_Media_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Mark_Media_Plugin
+ * @subpackage Mark_Media_Plugin/includes
+ * @author     Jack <jackc@markmedia.co>
+ */
+class Mark_Media_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
